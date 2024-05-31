580985347e3774239cb95c3df81b3584fc0f8826
kasra <kasra.hajian@divar.ir>
2024-05-31 20:56:47 +0330

    Add jupyter notebooks

--- Logic/core/link_analysis/graph.py
+++ Logic/core/link_analysis/graph.py
@@ -9,21 +9,16 @@ class LinkGraph:
     """
 
     def __init__(self):
-        # TODO
         self.graph = nx.DiGraph()
 
     def add_edge(self, u_of_edge, v_of_edge):
-        # TODO
         self.graph.add_edge(u_of_edge, v_of_edge)
 
     def add_node(self, node_to_add):
-        # TODO
         self.graph.add_node(node_to_add)
 
     def get_successors(self, node):
-        # TODO
         return list(self.graph.successors(node))
 
     def get_predecessors(self, node):
-        # TODO
         return list(self.graph.predecessors(node))
